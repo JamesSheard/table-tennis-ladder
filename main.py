51890a1d27c13d1974d6a330b95f2f9880f1f77a
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-09-07 16:41:27 +0100

    Adding state via text file

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class ladder:
     table = []
 
     def __init__(self):
-        self.table = [player("james"), player("mike"), player("john"), player("jim")]
+        self.read_state()
 
     def get_ladder(self):
         for player in self.table:
@@ -34,6 +34,24 @@ class ladder:
             return True
         return False
 
+    def read_state(self):
+        f = open("ladder/ladder_state.txt")
+        contents = f.read().split("\n")
+        for line in contents:
+            current_player = line.split()
+            if current_player:
+                self.table.append(player(current_player[0]))
+        f.close()
+
+    def write_state(self):
+        f = open("ladder/ladder_state.txt", "w")
+        f.truncate(0)
+        for player in self.table:
+            f.write(player.name + " " + str(self.table.index(player) + 1) + "\n")
+
+        f.close()
+        return
+
 
 def main():
     ladd = ladder() #main.py --win john --lose barry
@@ -41,6 +59,7 @@ def main():
         winner_name, loser_name = sys.argv[2], sys.argv[4]
         ladd.add_new_player(winner_name, loser_name)
         ladd.get_ladder()
+        ladd.write_state()
 
     elif sys.argv[1] == "--getladder":
         ladd.get_ladder()
