63387f27b731e04c28dfe4034056d854bed8cb18
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-09-11 11:53:40 +0100

    adding more test coverage

--- test_user_interface.py
+++ test_user_interface.py
@@ -7,49 +7,55 @@ class TestUserInterfaceMethods(unittest.TestCase):
     def setUp(self):
         self.user_interface = Interface([])
 
-    def test_unix_args_true(self):
-        self.user_interface.args = ["main.py", "--win", "James", "--lose", "Mike"]
-        expected_output = True
-        self.assertEqual(expected_output, self.user_interface.unix_args())
+    def test_unix_args(self):
+        test_cases = [
+            [["main.py", "--win", "James", "--lose", "mike"], True],
+            [["main.py", "-w", "James", "-l", "mike"], True],
+            [["main.py", "-w", "James", "--lose", "mike"], True],
+            [["foo", 0123, "bar"], False],
+            [[0123, 4385], False]
+        ]
 
-    def test_unix_args_false(self):
-        self.user_interface.args = ["lolol", 12342, "HFVHWVFVW"]
-        expected_output = False
-        self.assertEqual(expected_output, self.user_interface.unix_args())
+        for tc in test_cases:
+            self.user_interface.args = tc[0]
+            expected = tc[1]
+            self.assertEqual(expected, self.user_interface.unix_args())
 
-    def test_is_get_ladder_true(self):
-        self.user_interface.args = ["main.py", "--getladder"]
-        expected_output = True
-        self.assertEqual(expected_output, self.user_interface.is_get_ladder())
+    def test_is_get_ladder(self):
+        test_cases = [
+            [["main.py", "--getladder"], True],
+            [["main.py", "-gl"], True],
+            [["foo", "bar"], False]
+        ]
 
-    def test_is_get_ladder_false(self):
-        self.user_interface.args = ["main.py", "011899961991"]
-        expected_output = False
-        self.assertEqual(expected_output, self.user_interface.is_get_ladder())
+        for tc in test_cases:
+            self.user_interface.args = tc[0]
+            expected = tc[1]
+            self.assertEqual(expected, self.user_interface.is_get_ladder())
 
-    def test_is_get_help_true(self):
-        self.user_interface.args = ["main.py", "--help"]
-        expected_output = True
-        self.assertEqual(expected_output, self.user_interface.is_get_help())
+    def test_is_get_help(self):
+        test_cases = [
+            [["main.py", "--help"], True],
+            [["main.py", "-h"], True],
+            [["foo", "bar"], False]
+        ]
 
-    def test_is_get_help_false(self):
-        self.user_interface.args = ["main.py", "ugfugf"]
-        expected_output = False
-        self.assertEqual(expected_output, self.user_interface.is_get_help())
+        for tc in test_cases:
+            self.user_interface.args = tc[0]
+            expected = tc[1]
+            self.assertEqual(expected, self.user_interface.is_get_help())
 
-    def test_interactive_mode_true(self):
-        self.user_interface.args = ["main.py", "--interactive"]
-        expected_output = True
-        self.assertEqual(expected_output, self.user_interface.is_interactive_mode())
+    def test_interactive_mode(self):
+        test_cases = [
+            [["main.py", "--interactive"], True],
+            [["main.py", "-i"], True],
+            [["foo", "bar"], False]
+        ]
 
-    def test_interactive_mode_false(self):
-        self.user_interface.args = ["main.py", "Random"]
-        expected_output = False
-        self.assertEqual(expected_output, self.user_interface.is_interactive_mode())
-  
-
-
-
+        for tc in test_cases:
+            self.user_interface.args = tc[0]
+            expected = tc[1]
+            self.assertEqual(expected, self.user_interface.is_interactive_mode())
 
 
 if __name__ == '__main__':
